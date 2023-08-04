5ca71d1b67e1f542c2b9af4be18ab708a5aeda0e
alumno <alumno@Instituto.local>
2023-08-04 09:01:19 -0300

    uptade actividad

--- practica.js
+++ practica.js
@@ -1,8 +1,27 @@
-alert('hi barbie')
+console.log ('La suma es 12 + 2')
+console.log (12+2)
+console.log ('La resta es 42 - 2')
+console.log (42 - 2)
+console.log ('La multiplicación es 9 * 4')
+console.log (9 * 4)
+console.log ('La división es 15 / 3')
+console.log (15 / 3)
 
-console.log(150+8)
+let variableSuma = 12+2
+console.log (variableSuma)
+alert (variableSuma)
 
-let nombreAlumno = 'Sasha' 
-nombreAlumno = nombreAlumno + ' Cisneros'
+let variableResta = 42 - 2
+console.log (variableResta)
+alert (variableResta)
 
-alert (nombreAlumno)
+let variableMultiplicacion = 9 * 4
+console.log (variableMultiplicacion)
+alert (variableMultiplicacion)
+
+let variableDivision = 15 / 3
+console.log (variableDivision)
+alert (variableDivision)
+
+let miedad = (2023-2008)
+alert ('Mi edad es') + miedad
