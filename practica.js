47575df86f9d39bc2cfc968828df7255179845b5
Lou :) <136341659+louloou@users.noreply.github.com>
2023-08-07 00:20:20 -0300

    ultimas actividades

--- practica.js
+++ practica.js
@@ -6,6 +6,7 @@ console.log ('La multiplicación es 9 * 4')
 console.log (9 * 4)
 console.log ('La división es 15 / 3')
 console.log (15 / 3)
+// como agregar el texto adelante del número? 
 
 let variableSuma = 12+2
 console.log (variableSuma)
@@ -24,4 +25,31 @@ console.log (variableDivision)
 alert (variableDivision)
 
 let miedad = (2023-2008)
-alert ('Mi edad es') + miedad
+alert (miedad)
+// como agregar el texto adelante del número? 
+
+let añoUsuario = prompt ('Introduce tu año de nacimiento'); 
+let edadUsuario = (2023 - añoUsuario)
+alert ('Tu edad es '+ edadUsuario)
+
+let nombre = prompt ('Introduce tu Nombre'); 
+alert ('Bienvenido/a mi página web, ' + nombre)
+
+let nombreAlumno = 'Lourdes '
+let apellidoAlumno = 'Bray Caballero'
+let yo = nombreAlumno+apellidoAlumno
+alert ('Hola, '+ yo)
+console.log ('Hola, ' + yo)
+
+let diez = 10
+let tres = 3
+let resultadoMultiplicacion = 10 * 3
+console.log ('Resultado 10*3= ' + resultadoMultiplicacion)
+
+
+
+
+
+
+
+
