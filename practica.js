527335a477f2e8ab2611773268a537b5fad43456
alumno <alumno@Instituto.local>
2023-08-07 10:08:50 -0300

    Update practica.js

--- practica.js
+++ practica.js
@@ -1,46 +1,45 @@
 
-console.log ('El resultado de la suma es: ' + (12+2))
-console.log ('El resultado de la resta es: '+ (42 - 2))
-console.log ('El resultado de la multiplicacion es: ' +(9 * 4))
-console.log ('El resultado de la division es: ' + (15 / 3)) 
+console.log('El resultado de la suma es: ' + (12 + 2))
+console.log('El resultado de la resta es: ' + (42 - 2))
+console.log('El resultado de la multiplicacion es: ' + (9 * 4))
+console.log('El resultado de la division es: ' + (15 / 3))
 
-let variableSuma = 12+2
-console.log (variableSuma)
-alert (variableSuma)
+let variableSuma = 12 + 2
+console.log(variableSuma)
+alert(variableSuma)
 
 let variableResta = 42 - 2
-console.log (variableResta)
-alert (variableResta)
+console.log(variableResta)
+alert(variableResta)
 
 let variableMultiplicacion = 9 * 4
-console.log (variableMultiplicacion)
-alert (variableMultiplicacion)
+console.log(variableMultiplicacion)
+alert(variableMultiplicacion)
 
 let variableDivision = 15 / 3
-console.log (variableDivision)
-alert (variableDivision)
+console.log(variableDivision)
+alert(variableDivision)
 
-let miedad = (2023-2008)
-alert ('Mi edad es: ' + miedad)
-// como agregar el texto adelante del número? 
+let miedad = (2023 - 2008)
+alert('Mi edad es: ' + miedad)
 
-let añoUsuario = prompt ('Introduce tu año de nacimiento'); 
+let añoUsuario = prompt('Introduce tu año de nacimiento');
 let edadUsuario = (2023 - añoUsuario)
-alert ('Tu edad es '+ edadUsuario)
+alert('Tu edad es ' + edadUsuario)
 
-let nombre = prompt ('Introduce tu Nombre'); 
-alert ('Bienvenido/a mi página web, ' + nombre)
+let nombre = prompt('Introduce tu Nombre');
+alert('Bienvenido/a mi página web, ' + nombre)
 
 let nombreAlumno = 'Lourdes '
 let apellidoAlumno = 'Bray Caballero'
-let yo = nombreAlumno+apellidoAlumno
-alert ('Hola, '+ yo)
-console.log ('Hola, ' + yo)
+let yo = nombreAlumno + apellidoAlumno
+alert('Hola, ' + yo)
+console.log('Hola, ' + yo)
 
 let diez = 10
 let tres = 3
 let resultadoMultiplicacion = 10 * 3
-console.log ('Resultado 10*3= ' + resultadoMultiplicacion)
+console.log('Resultado 10*3= ' + resultadoMultiplicacion)
 
 
 
