9f171180faee366643637965958b7b019d85e3de
alumno <alumno@Instituto.local>
2023-10-27 09:32:26 -0300

    27 del 10

---
+++ tpCondicionales.js
@@ -0,0 +1,30 @@
+let nombre = prompt("Ingresa tu nombre")
+function saludoBienvenida(nombre) {
+    if ((nombre == "Nahuel") || (nombre == "Santino")) {
+        alert("No sos bienvenido")
+        return nombre
+    } else {
+        alert("Bienvenido, " + nombre)
+        return nombre
+    }
+}
+saludoBienvenida(nombre)
+
+let edad = prompt("Ingresa tu edad")
+function esMayor(edad) {
+    if (edad >= 18) {
+        alert("Sos mayor de edad")
+    } else {
+        alert("No sos mayor de edad")
+    }
+}
+esMayor(edad)
+
+let edadCat = prompt("Ingresa tu edad")
+function categoriaEdad (edadCat) {
+if (edadCat <= 11) {
+    alert ("sos un niño")
+} else if (edadCat <= 18) {
+    alert ("Adolescente")
+} else if (edad <= 26){  
+alert ("joven-adulto")
