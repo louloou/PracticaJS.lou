0fa253fcaa8c4e4590491bb17260f593399345ed
alumno <49128509@ivt.edu.ar>
2023-11-03 09:28:07 -0300

    up

--- dom.js
+++ dom.js
@@ -1,4 +1,13 @@
 // Buscar elemeto h1 y guardar en variable
 let titulo = document.querySelector("h1")
 // modificar texto del elemento h1
-titulo.innerText = "Hola como estan"
+
+
+let imagen = document.querySelector(".Imagen")
+imagen.src = "chori.jfif"
+
+let boton = document.querySelector("#cambiarImagen")
+boton.onclick = function() {
+    titulo.innerText = "Hola como estan"
+    imagen.src = "chori.jfif"
+}
